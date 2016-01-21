49e2e1037c59ee8d4fa9c811faf5565cf597e7aa
m00n <arcybartek@gmail.com>
2016-01-21 09:19:44 +0100

    added missile origin point to gun model

--- src/app/gun.js
+++ src/app/gun.js
@@ -5,17 +5,16 @@ function Gun(game) {
     gun.model = null;
 
     gun.setPosition = function() {
-        gun.model.scene.position.x = 0.5;
-        gun.model.scene.position.y = -1;
-        gun.model.scene.position.z = 1.5; //
+        gun.model.scene.position.x = 0.5; // positive: right, negative: left
+        gun.model.scene.position.y = -1; // positive: up, negative: down
+        gun.model.scene.position.z = 1.5; // positive: behind, negative: forward
         gun.model.scene.rotation.y = - 2 * (Math.PI / 2);
-        game.playerCamera.camera.add(gun.model.scene);
-
     };
 
     gun.onAllModelsLoadedListener = function() {
         gun.model = game.models.fetch("gun");
         gun.setPosition();
+        game.playerCamera.camera.add(gun.model.scene);
     };
 
     gun.spawn = function() {
